603de6cf9bdf96fbb41518d38891ffe59c6e8621
chungliqing <chungliqing@live.com>
2024-04-24 09:27:04 -0400

    pending writeup

---
+++ picoCTF/Reverse Engineering/timer/timer.apk.cache/sources/3f/00000d3f.java
@@ -0,0 +1,36 @@
+package com.google.android.material.progressindicator;
+
+import android.animation.Animator;
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat;
+
+/* loaded from: classes.dex */
+public abstract class IndeterminateAnimatorDelegate<T extends Animator> {
+    protected IndeterminateDrawable drawable;
+    protected final int[] segmentColors;
+    protected final float[] segmentPositions;
+
+    public abstract void cancelAnimatorImmediately();
+
+    public abstract void invalidateSpecValues();
+
+    public abstract void registerAnimatorsCompleteCallback(Animatable2Compat.AnimationCallback animationCallback);
+
+    public abstract void requestCancelAnimatorAfterCurrentCycle();
+
+    public abstract void startAnimator();
+
+    public abstract void unregisterAnimatorsCompleteCallback();
+
+    public IndeterminateAnimatorDelegate(int segmentCount) {
+        this.segmentPositions = new float[segmentCount * 2];
+        this.segmentColors = new int[segmentCount];
+    }
+
+    public void registerDrawable(IndeterminateDrawable drawable) {
+        this.drawable = drawable;
+    }
+
+    public float getFractionInRange(int playtime, int start, int duration) {
+        return (playtime - start) / duration;
+    }
+}
